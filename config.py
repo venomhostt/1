319067f9388be1360cfb2493978c664d0fc37a05
Venom <venomhost.in@gmail.com>
2025-12-20 17:17:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,15 +35,15 @@ API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or b
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/venomhostt/IpsitaMusic",
+    "https://github.com/venomhostt/1",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/venomvipbots") 
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/venompratapchat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+1BsWi0e07i83MDhl") 
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+1BsWi0e07i83MDhl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -120,6 +120,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
